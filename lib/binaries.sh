6e7826f6882b4388a37124d5020ca999abd44525
Jason Zigelbaum <jason.zigelbaum@pod1.com>
2015-09-12 17:17:11 -0400

    update.

--- lib/binaries.sh
+++ lib/binaries.sh
@@ -62,5 +62,56 @@ install_npm() {
 
 install_imagemagick() {
   echo "Installing imagmagick"
-  apt-get install imagemagick
+  
+  # Fail fast and fail hard.
+  set -e
+
+  # Prepend proper path for virtualenv hackery. This will be deprecated soon.
+  export PATH=:/usr/local/bin:$PATH
+
+  # Paths.
+  BIN_DIR=$(cd $(dirname $0); pwd) # absolute path
+  ROOT_DIR=$(dirname $BIN_DIR)
+  BUILD_DIR=$1
+  CACHE_DIR=$2
+
+  # We'll need to send these statics to other scripts we `source`.
+  # export PIP_DOWNLOAD_CACHE
+  export BUILD_DIR
+
+  # Syntax sugar.
+
+  function puts-step (){
+    echo "-----> $@"
+  }
+
+  # ## Build Time
+  #
+
+  # Switch to the repo's context.
+  cd $BUILD_DIR
+
+  IM_LOCATION="im"
+
+  mkdir -p $IM_LOCATION
+
+  # Install ImageMagick
+  IM_VERSION="6.8.6-6"
+  IM_URL="https://assets.tandemstock.com.s3.amazonaws.com/im-$IM_VERSION.tar.gz"
+  puts-step "Bundling ImageMagick version $IM_VERSION"
+  curl --silent --max-time 120 --location "$IM_URL" | tar xz
+
+  # Install GraphicsMagick
+  GM_VERSION="1.3.18"
+  GM_URL="https://assets.tandemstock.com.s3.amazonaws.com/GraphicsMagick-$GM_VERSION.tar.gz"
+  puts-step "Bundling GraphicsMagick version $GM_VERSION"
+  curl --silent --max-time 120 --location "$GM_URL" | tar xz
+
+  # ufraw dependency
+  UFRAW_VERSION="0.19"
+  UFRAW_URL="https://assets.tandemstock.com.s3.amazonaws.com/ufraw-0.19.tar.gz"
+  puts-step "Bundling ufraw version $UFRAW_VERSION"
+  curl --silent --max-time 60 --location "$UFRAW_URL" | tar xz -C $IM_LOCATION
+
+  export PATH=:/app/im/bin:/local/bin:$PATH
 }
